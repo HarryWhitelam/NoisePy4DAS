28f725e1661e19642f5b71e47cfc2029e9f3a374
Harry Whitelam <gfs19eku@uea.ac.uk>
2024-10-30 14:26:51 +0000

    Commit to merge with main changes

--- scripts/correlation.py
+++ scripts/correlation.py
@@ -22,21 +22,21 @@ corrs = []
 # freq_range = [[0.01, 0.04], [0.04, 0.1], [0.1, 1.0], [1.0, 49.9]]
 # n_minute_range = [10, 60, 120, 360]
 # target_spatial_res_range = [0.25, 1, 5, 10]
-channels_range = [[2000, 3499], [3500, 4999], [5000, 6499], [6500, 7999]]
-for chas in channels_range:
-    print(f'Starting channels experiment: {chas}')
-    prepro_para = set_prepro_parameters(dir_path, cha1=chas[0], cha2=chas[1], target_spatial_res=5)
-    tdms_array, timestamps = get_tdms_array(dir_path)
-    corr_full = correlation(tdms_array, prepro_para, timestamps, task_t0)
-    corrs.append(corr_full)
+# channels_range = [[2000, 3499], [3500, 4999], [5000, 6499], [6500, 7999]]
+# for chas in channels_range:
+#     print(f'Starting channels experiment: {chas}')
+#     prepro_para = set_prepro_parameters(dir_path, cha1=chas[0], cha2=chas[1], target_spatial_res=5)
+#     tdms_array, timestamps = get_tdms_array(dir_path)
+#     corr_full = correlation(tdms_array, prepro_para, timestamps, task_t0)
+#     corrs.append(corr_full)
 
-plot_multiple_correlations(corrs, prepro_para, channels_range, save_corr=False)
+# plot_multiple_correlations(corrs, prepro_para, channels_range, save_corr=False)
 
 
 
 ### SINGLE RUN: 
-# prepro_para = set_prepro_parameters(dir_path, target_spatial_res=1)
-# tdms_array, timestamps = get_tdms_array()
+prepro_para = set_prepro_parameters(dir_path, target_spatial_res=1)
+tdms_array, timestamps = get_tdms_array()
 
-# corr_full = correlation(tdms_array, prepro_para, timestamps, task_t0, save_corr=False)
-# plot_correlation(corr_full, prepro_para)
+corr_full = correlation(tdms_array, prepro_para, timestamps, task_t0, save_corr=False)
+plot_correlation(corr_full, prepro_para)
