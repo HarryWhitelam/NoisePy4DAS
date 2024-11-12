c273033c77e5bef251b7cd06ee35e84ca549c60d
Harry Whitelam <gfs19eku@uea.ac.uk>
2024-11-12 12:30:44 +0000

    Commit local changes (should be the same pretty much as next pull)

--- scripts/correlation.py
+++ scripts/correlation.py
@@ -26,7 +26,7 @@ def channels_experiment():
 
 def frequency_experiment():
     corrs = []
-    freq_range = [[0.01, 1.0], [1.0, 5.0], [5.0, 10.0], [10.0, 15.0], [15.0, 20.0], [20.0, 25.0]]
+    freq_range = [[0.001, 1.0], [1.0, 5.0], [5.0, 10.0], [10.0, 15.0], [15.0, 20.0], [20.0, 25.0]]
     
     for freqs in freq_range:
         print(f'Frequency experiment band: {freqs}')
